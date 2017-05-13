d796fc3e711a40a939d53f0e6595913bff79779b
alexanderFeltser <alexanderfster@gmail.com>
2017-05-13 16:08:24 +0300

    Third line

---
+++ TestGit/src/TestGit.java
@@ -0,0 +1,8 @@
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("First line");
+		System.out.println("Second line , after commit");
+		System.out.println("third line ");
+	}
+}
